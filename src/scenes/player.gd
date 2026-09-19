d2127d6b846ad28c2d91dd2a809f0c0eb1ba00c1
A-Rjs <ark09.stone@gmail.com>
2026-09-18 23:11:37 -0400

    speed adjustments

--- src/scenes/player.gd
+++ src/scenes/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 600.0
 const JUMP_VELOCITY = -400.0
-const BOUNCE_VELOCITY = -750.0
+const BOUNCE_VELOCITY = -840.0
 
 
 func _physics_process(delta: float) -> void:
@@ -22,7 +22,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED/1.5
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
